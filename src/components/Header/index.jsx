d2e50826021bb4d5349ced126fe7edc95f37ec41
Germain Bonnard <germain80420@gmail.com>
2023-08-16 20:44:25 +0200

    modif header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,29 +1,14 @@
-import { Link, useLocation } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import "../../styles/Header.css"
 import logo from "../../assets/logo.png"
 
 function Header() {
-  const location = useLocation()
-  console.log(location.pathname)
-  function getClassName(name) {
-    if (location.pathname === name) {
-      return "liNavActive"
-    } else {
-      return "liNav"
-    }
-  }
-
   return (
     <div className="header">
       <img src={logo} alt="logo" className="imgLogo" />
       <nav className="navHeader">
-        <Link className={getClassName("/")} to="/">
-          Acceuil
-        </Link>
-
-        <Link className={getClassName("/a_propos")} to="/a_propos">
-          A Propos
-        </Link>
+        <NavLink to="/">Accueil</NavLink>
+        <NavLink to="/a_propos">A Propos</NavLink>
       </nav>
     </div>
   )
